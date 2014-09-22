4fa377895f4c33b0c4d7f7c00c5d0e424ac53b54
bcole <333qaz333@gmail.com>
2014-09-22 15:46:22 -0400

    Added functionality to discover URL Query Params and print them out at the end

--- Fuzzer/src/Discover.java
+++ Fuzzer/src/Discover.java
@@ -1,5 +1,8 @@
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.util.List;
@@ -8,6 +11,7 @@ import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
@@ -22,6 +26,8 @@ public class Discover {
 	private ArrayList<String> av = new ArrayList<String>();
 	// List of links that need to be visited
 	private ArrayList<String> visit = new ArrayList<String>();
+	// List of query inputs discovered
+	private HashMap<String, Set<String>> queryInputs = new HashMap<String, Set<String>>();
 	
 	public Discover(String url, String custom_auth, String common_words){
 		this.custom_auth = custom_auth;
@@ -48,6 +54,18 @@ public class Discover {
 			searchSub(visit.get(0));
 			visit.remove(0);
 		}
+		
+		if(queryInputs.keySet().size() != 0) {
+			// Print out collected query inputs
+			System.out.println();
+			System.out.println("Gathered URL Query Inputs:");
+			for(String key : queryInputs.keySet()) {
+				System.out.println("     For URL: " + key);
+				for(String q : queryInputs.get(key)) {
+					System.out.println("       Param: " + q);
+				}
+			}
+		}
 		System.out.println("finished");
 		
 	}
@@ -55,13 +73,24 @@ public class Discover {
 		try {
 			System.out.println("\nIn search: "+inaddress);
 			
+			// Load the requested page
 			HtmlPage page = webClient.getPage(inaddress);
+			
+			// Forms discovery
 			List<HtmlForm> forms = page.getForms();
 			for (HtmlForm form : forms) {
 				String id = form.getId();
 				if("".equals(id)) id = "(no ID for form) " + form.getCanonicalXPath();
-				System.out.println("     Input discovered: " + id );
+				System.out.println("     Form discovered: " + id );
+				List<HtmlElement> inputs = form.getHtmlElementsByTagName("input");
+				for(HtmlElement input : inputs){
+					id = input.getAttribute("name");
+					if("".equals(id)) continue;	// input must have name to be an input
+					System.out.println("       Input discovered: " + id );
+				}
 			}
+			
+			// Links discovery
 			List<HtmlAnchor> links = page.getAnchors();
 			String url;
 			for (HtmlAnchor link : links) {
@@ -69,12 +98,23 @@ public class Discover {
 				if(!page.getFullyQualifiedUrl(link.getHrefAttribute()).getHost().equals(address.getHost())) {
 					continue;
 				}
-				url = page.getFullyQualifiedUrl(link.getHrefAttribute()).toString();
+				URL urlObj = page.getFullyQualifiedUrl(link.getHrefAttribute());
+				url = urlObj.toString();
+				
+				// Does URL have query inputs?
+				if(urlObj.getQuery() != null) {
+					url = url.replace('?' + urlObj.getQuery(), "");
+					Set<String> qs;
+					if(queryInputs.containsKey(url)) {
+						qs = queryInputs.get(url);
+					} else {
+						qs = new HashSet<String>();
+					}
+					qs.add(urlObj.getQuery());
+					queryInputs.put(url, qs);
+				}
 				
 				// Have we already visited this url?
-				if(url.contains("dvwa")){
-					System.out.println("breakpoint");
-				}
 				if(av.contains(url)) continue;
 				
 				// New internal URL!
